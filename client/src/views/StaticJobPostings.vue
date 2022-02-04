11dae306055243f5fa9754ee7c2ad15cdf04f59d
Kaline Costa <kaline.economia@gmail.com>
2022-02-04 00:40:51 -0300

    Button e Modal - StaticJobPostings

--- client/src/views/StaticJobPostings.vue
+++ client/src/views/StaticJobPostings.vue
@@ -2,20 +2,52 @@
   <div>
     <h4>Job Posting Title</h4>
     <b-button v-b-modal.modal-job-posting-detail>Details</b-button>
-    <b-modal id="modal-job-posting-detail" title="<Title>Details" hide-footer>
-      <p class="my-4"><b>Office:</b> office</p>
-      <p class="my-4"><b>Status:</b> status</p>
-      <p class="my-4"><b>Description:</b> description</p>
-      <p class="my-4"><b>Salary Range:</b> salary range</p>
-      <p class="my-4"><b>Benefits:</b> benefits</p>
-    </b-modal>
-    <b-modal id="modal-candidate-detail" title=" <Name> Details" hide-footer>
-      <p class="my-4"><b>Address:</b> address</p>
-      <p class="my-4"><b>Email:</b> email</p>
-      <p class="my-4"><b>Applied to:</b> applied to</p>
-      <p class="my-4"><b>Phone:</b> phone</p>
-      <p class="my-4"><b>Linkedin:</b> linkedin</p>
-    </b-modal>
+    <b-modal id="modal-job-posting-detail" hide-footer>
+      <template v-slot:modal-header>
+        <h4 class="modal-title">
+            <b>Title Details</b>
+        </h4>
+        <div class="w-100">
+            <b-button variant="light border-dark"
+              class="btn btn-primary action_btn float-right" v-on:click= "closeModal">
+                Edit
+            </b-button>
+          </div>
+  </template>
+        <p class="my-4"><b>Office:</b> office</p>
+        <p class="my-4"><b>Status:</b> status</p>
+        <p class="my-4"><b>Description:</b> description</p>
+        <p class="my-4"><b>Salary Range:</b> salary range</p>
+        <p class="my-4"><b>Benefits:</b> benefits</p>
+      </b-modal>
+      <b-modal id="modal-candidate-detail" hide-footer>
+  <template v-slot:modal-header>
+      <h4 class="modal-title">
+        <b>Name Details</b>
+      </h4>
+        <div class="w-100">
+          <b-button variant="light border-dark"
+            class="btn btn-primary action_btn float-right" v-on:click="closeModal">
+             Edit
+          </b-button>
+        </div>
+      </template>
+        <p class="my-4"><b>Office:</b> office</p>
+        <p class="my-4"><b>Status:</b> status</p>
+        <p class="my-4"><b>Description:</b> description</p>
+        <p class="my-4"><b>Salary Range:</b> salary range</p>
+        <p class="my-4"><b>Benefits:</b> benefits</p>
+  </b-modal>
+      <b-modal id="modal-move-candidate" title="<Name>" ok-title="Move">
+      <p class="my-4"><b>Current stage:</b> stage</p>
+        <b>Move to:</b>
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+      </select>
+  </b-modal>
     <b-card no body>
           <b-tabs card>
           <b-tab title="Applied" active>
@@ -37,8 +69,8 @@
                   <td>December 15, 1815</td>
                 <td>
                   <b-button>Archive</b-button>
-                  <b-button>Move</b-button>
-                  <b-button v-b-modal.modal-candidates-detail></b-button>
+                  <b-button v-b-modal.modal-move-candidate>Details</b-button>
+                  <b-button v-b-modal.modal-candidate-detail>Details</b-button>
                 </td>
               </tr>
               <tr>
@@ -48,8 +80,8 @@
                   <td>December 09, 1906</td>
                 <td>
                   <b-button>Archive</b-button>
-                  <b-button>Move</b-button>
-                  <b-button>Details</b-button>
+                  <b-button v-b-modal.modal-move-candidate>Details</b-button>
+                  <b-button v-b-modal.modal-candidate-detail>Details</b-button>
                 </td>
               </tr>
                 <tr>
@@ -59,8 +91,8 @@
                   <td>December 17, 1936</td>
                 <td>
                   <b-button>Archive</b-button>
-                  <b-button>Move</b-button>
-                  <b-button>Details</b-button>
+                  <b-button v-b-modal.modal-move-candidate>Details</b-button>
+                  <b-button v-b-modal.modal-candidate-detail>Details</b-button>
                 </td>
                 </tr>
                 <tr>
@@ -70,8 +102,8 @@
                   <td>June 24, 1917</td>
                 <td>
                   <b-button>Archive</b-button>
-                  <b-button>Move</b-button>
-                  <b-button>Details</b-button>
+                  <b-button v-b-modal.modal-move-candidate>Details</b-button>
+                  <b-button v-b-modal.modal-candidate-detail>Details</b-button>
                 </td>
               </tr>
             </tbody>
