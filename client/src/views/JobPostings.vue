1fd07b838e181ad90d7c791020f97ce2ffd60a5d
Kaline Costa <kaline.economia@gmail.com>
2022-02-10 10:20:12 -0300

    JobPostinging_acrescimo de itens

--- client/src/views/JobPostings.vue
+++ client/src/views/JobPostings.vue
@@ -4,7 +4,19 @@
       <b-tabs card>
         <!-- Render Tabs, supply a unique `key` to each tab -->
           <b-tab v-for="tab_name in tabs" :key="'dyn-tab-' + tab_name" :title="tab_name">
-             <b-table striped hover :items="items"></b-table>
+            <b-table :items="items" :fields="fields" striped responsive="sm">
+          <template #cell(actions)>
+              <b-button size="sm" class="mr-2">
+              Archive
+              </b-button>
+              <b-button size="sm" class="mr-2">
+              Move
+              </b-button>
+              <b-button size="sm" class="mr-2">
+              Details
+              </b-button>
+            </template>
+          </b-table>
         </b-tab>
       </b-tabs>
    </b-card>
@@ -16,6 +28,7 @@ export default {
   data() {
     return {
       tabs: ['Applied', 'Resume Analysis', 'Contacted', 'Interview'],
+      fields: ['name', 'appliede on', 'stage', 'last_modified_on', 'actions'],
       items: [
         {
           Name: 'Michelangelo',
