02d8c0c50384bdefd354d4672253011b532cdac3
Kaline Costa <kaline.economia@gmail.com>
2022-02-12 23:14:36 -0300

    StaticJobPostings_Button_Modal

--- client/src/views/StaticJobPostings.vue
+++ client/src/views/StaticJobPostings.vue
@@ -1,6 +1,57 @@
 <template>
   <div>
-    <b-card no-body>
+    <h4>Job Posting Title</h4>
+    <b-button v-b-modal.modal-job-posting-detail>Details</b-button>
+    <b-modal id="modal-job-posting-detail" hide-footer>
+      <template v-slot:modal-header>
+        <h4 class="modal-title">
+          <b>Title Details</b>
+        </h4>
+        <div class="w-100">
+            <b-button variant="light border-dark" class="btn btn-primary action_btn float-right"
+              v-on:click="closeModal">
+              Edit
+            </b-button>
+        </div>
+    </template>
+      <p class="my-4"><b>Office:</b>office</p>
+      <p class="my-4"><b>Status:</b> status</p>
+      <p class="my-4"><b>Description:</b> description</p>
+      <p class="my-4"><b>Salary Range:</b> salary range min to salary range max</p>
+      <p class="my-4"><b>Benefits:</b> benefits</p>
+      <p class="my-4"><b>Hiring Type:</b> hiring type</p>
+    </b-modal>
+    <b-modal id="modal-candidate-detail" hide-footer>
+      <template v-slot:modal-header>
+      <h4 class="modal-title">
+          <b>Name Details</b>
+        </h4>
+        <div class="w-100">
+            <b-button variant="light border-dark" class="btn btn-primary action_btn float-right"
+              v-on:click="closeModal">
+              Edit
+            </b-button>
+        </div>
+    </template>
+      <p class="my-4"><b>Adress:</b> adress</p>
+      <p class="my-4"><b>Email:</b> email</p>
+      <p class="my-4"><b>Applied to:</b> aplied to</p>
+      <p class="my-4"><b>Phone:</b> phone</p>
+      <p class="my-4"><b>Linkedin:</b> linkedin</p>
+    </b-modal>
+     <b-modal id="modal-move-candidate" title="<Name>" ok-title="Move">
+      <p class="my-4"><b>Current stage:</b> stage</p>
+      <p class="my-4">
+        <b>Move to:</b>
+        </p>
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+    </b-modal>
+  <b-card no-body>
       <b-tabs card>
       <b-tab title="Applied" active>
         <table class="table">
@@ -21,8 +72,8 @@
             <td>December 10, 1815</td>
             <td>
               <b-button>Archive</b-button>
-              <b-button>Move</b-button>
-              <b-button>Details</b-button>
+              <b-button v-b-modal.modal-move-candidate>Move </b-button>
+              <b-button v-b-modal.modal-candidate-detail>Details </b-button>
             </td>
           </tr>
           <tr>
@@ -32,8 +83,8 @@
             <td>December 9, 1906</td>
             <td>
               <b-button>Archive</b-button>
-              <b-button>Move</b-button>
-              <b-button>Details</b-button>
+              <b-button v-b-modal.modal-move-candidate>Move </b-button>
+              <b-button v-b-modal.modal-candidate-detail>Details </b-button>
             </td>
           </tr>
           <tr>
@@ -43,8 +94,8 @@
             <td>August 17, 1936</td>
             <td>
               <b-button>Archive</b-button>
-              <b-button>Move</b-button>
-              <b-button>Details</b-button>
+              <b-button v-b-modal.modal-move-candidate>Move </b-button>
+              <b-button v-b-modal.modal-candidate-detail>Details </b-button>
             </td>
           <tr>
             <td scope="row">Joan Clarke</td>
@@ -53,8 +104,8 @@
             <td>June 24, 1917</td>
             <td>
               <b-button>Archive</b-button>
-              <b-button>Move</b-button>
-              <b-button>Details</b-button>
+              <b-button v-b-modal.modal-move-candidate>Move </b-button>
+              <b-button v-b-modal.modal-candidate-detail>Details </b-button>
             </td>
           </tr>
         </tbody>
